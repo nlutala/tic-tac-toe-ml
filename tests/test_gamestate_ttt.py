ee1892efad951da6a5e2dafffe91e24e74373f37
nlutala <nathanlutala@outlook.com>
2024-06-23 17:19:18 +0100

    Update test_gamestate_ttt.py to remove unnecessary comment

--- tests/test_gamestate_ttt.py
+++ tests/test_gamestate_ttt.py
@@ -9,17 +9,6 @@ from secrets import choice
 import unittest
 
 class TestGameState(unittest.TestCase):
-    '''
-    def test_make_move_returns_index_that_is_not_already_taken(self):
-        indexes_to_add_os = list(set([random.randint(0,8) for i in range(4)]))
-        game_state = ["" for i in range(9)]
-
-        for index in indexes_to_add_os:
-            game_state[index] = "O"
-
-        naive_cpu = NaiveCPU("X")
-        assert game_state[naive_cpu.make_move(game_state)] == ""
-    '''
     def test_gamestate_raises_value_error_if_player_tries_to_put_symbol_on_taken_place(self):
         gs = GameState()
 
