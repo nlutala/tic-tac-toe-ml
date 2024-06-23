f71518f0f9924150a1e804a868a7787bdf4e9a7d
nlutala <nathanlutala@outlook.com>
2024-06-23 16:24:48 +0100

    Update gamestate_ttt.py to include checks for if the game is done and writing the results to a text file

--- gamestate_ttt.py
+++ gamestate_ttt.py
@@ -6,16 +6,23 @@ for the version of the CPU that uses a ML model.
 
 Nathan Lutala (nlutala)
 '''
+import os
+
 class GameState:
-    def __init__(self) -> None:
+    def __init__(self, write_to_file=True) -> None:
         '''
         self.outcome will either be set to:
         W (for win), D (for draw), L (loss)
         from the CPU's perspective
+
+        Parameter
+        write_to_file (bool) - optional boolean value for whether to write to a
+        text file
         '''
         self.outcome = ""
         # Set up the grid for the tic-tac-toe game
         self.game_state = ["" for i in range(9)]
+        self.write_to_file = write_to_file
 
     def get_game_state(self) -> list:
         return self.game_state
@@ -33,3 +40,68 @@ class GameState:
             raise ValueError(f"This position has already been taken. Place an {symbol} where there is a free space.")
         else:
             self.game_state[index] = symbol
+
+    def is_done(self) -> bool:
+        '''
+        Method to determine whether the game is done and who won.
+
+        Returns True if there is a winner or all parts of the grid
+        have been filled out. False otherwise
+        '''
+        if self.game_state[0] == self.game_state[1] and self.game_state[1] == self.game_state[2] and self.game_state[0] != "":
+            if self.game_state[0] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[3] == self.game_state[4] and self.game_state[4] == self.game_state[5] and self.game_state[3] != "":
+            if self.game_state[3] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[6] == self.game_state[7] and self.game_state[7] == self.game_state[8] and self.game_state[6] != "":
+            if self.game_state[6] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[0] == self.game_state[3] and self.game_state[3] == self.game_state[6] and self.game_state[0] != "":
+            if self.game_state[0] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[1] == self.game_state[4] and self.game_state[4] == self.game_state[7] and self.game_state[1] != "":
+            if self.game_state[1] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[2] == self.game_state[5] and self.game_state[5] == self.game_state[8] and self.game_state[2] != "":
+            if self.game_state[2] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[0] == self.game_state[4] and self.game_state[4] == self.game_state[8] and self.game_state[0] != "":
+            if self.game_state[0] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state[2] == self.game_state[4] and self.game_state[4] == self.game_state[6] and self.game_state[2] != "":
+            if self.game_state[2] == "X":
+                self.outcome = "W"
+            else:
+                self.outcome = "L"
+        elif self.game_state.count("") == 0:
+            self.outcome = "D"
+        else:
+            return False
+        
+        return True
+
+    def _write_results_to_file(self) -> None:
+        '''
+        Private function to write the results of the game to a file
+        '''
+        if "game_results_ttt.txt" not in os.listdir():
+            with open("game_results_ttt.txt", "w") as file:
+                file.write("".join(self.game_state) + self.outcome)
+        else:
+            with open("game_results_ttt.txt", "a") as file:
+                file.write("".join(self.game_state) + self.outcome)
